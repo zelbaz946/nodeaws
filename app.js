76b3eab020d42a4345844e85e8357918b63c697e
Ziyad E. El Baz <ziyad.elbaz@me.com>
2021-03-31 18:06:30 -0600

    update

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.get('/', (req, res) => {
 
 const port = process.env.port || 3000
 app.listen(port, () => {
-  console.log('Server is listening')
+  console.log('Server is listening and kicking')
 });
 
